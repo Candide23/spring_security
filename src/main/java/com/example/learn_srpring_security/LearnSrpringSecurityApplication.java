dd5279bb1ddbdacc8accf5525d929df02a5aa118
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-11-18 14:18:34 -0600

    first commit

---
+++ src/main/java/com/example/learn_srpring_security/LearnSrpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_srpring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSrpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSrpringSecurityApplication.class, args);
+	}
+
+}
