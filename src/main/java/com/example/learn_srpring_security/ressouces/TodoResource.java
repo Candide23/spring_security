a9a7f386b5b7a63a0cd47d39cf1768fb977da587
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-11-19 14:43:23 -0600

    added some exmaples

---
+++ src/main/java/com/example/learn_srpring_security/ressouces/TodoResource.java
@@ -0,0 +1,41 @@
+package com.example.learn_srpring_security.ressouces;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private static final List<Todo> TODOS_LIST =
+            List.of(new Todo("in28minutes", "Learn AWS"),
+                    new Todo("in28minutes", "Get AWS Certified"));
+
+
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos() {
+        return TODOS_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForSpecificUser(@PathVariable("username") String username) {
+        return TODOS_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForSpecificUser(@PathVariable String username
+            , @RequestBody Todo todo) {
+        logger.info("Create {} for {}", todo, username);
+    }
+
+    record Todo (String username, String description) {
+
+    }
+
+
+}
+
